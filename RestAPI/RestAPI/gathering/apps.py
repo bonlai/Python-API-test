5bbd0a83d82689f556af3297d2450164f3a06213
bonlai <bonlai888@hotmail.com>
2018-01-04 17:56:16 +0800

    delete unuse folder

--- RestAPI/RestAPI/gathering/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GatheringConfig(AppConfig):
-    name = 'gathering'
--- RestAPI/RestAPI/images/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Image(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    image=models.ImageField(upload_to='Images/', default='Images/None/No-img.jpg')
-
-    class Meta:
-        db_table = "image"
