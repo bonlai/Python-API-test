c0607a432f83516e344090ebc90712c0bffa416b
bonlai <bonlai888@hotmail.com>
2018-01-04 17:52:25 +0800

    delete music folder
    delete music folder

--- RestAPI/RestAPI/musics/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from musics.models import Music
-
-
-class MusicSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Music
-        # fields = '__all__'
-        fields = ('id', 'song', 'singer', 'last_modify_date', 'created','url')
